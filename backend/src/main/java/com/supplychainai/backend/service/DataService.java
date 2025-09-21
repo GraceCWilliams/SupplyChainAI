740bad2230c00de30e3e8cbc86ea4c6d2b0b32e4
Grace Williams <grwilliams@g.hmc.edu>
2025-09-21 01:01:38 -0700

    deleted folders

--- backend/src/main/java/com/supplychainai/backend/service/DataService.java
+++
@@ -1,8 +0,0 @@
-package com.supplychainai.backend.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class DataService {
-    // Future logic: time series, optimization
-}
